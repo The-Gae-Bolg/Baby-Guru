365454c46bd0ce16e00ea43786d2192fdfe4aca4
The-Gae-Bolg <65456792+The-Gae-Bolg@users.noreply.github.com>
2020-05-16 17:18:56 -0500

    add whatsapp
    cambiar el numero de telefono en el whatsapp.js

---
+++ Baby Guru/script/whatsapp.js
@@ -0,0 +1,12 @@
+    $(function () {
+           $('#WAButton').floatingWhatsApp({
+               phone: '5213781080994', 
+               headerTitle: 'Chat de Baby Guru', 
+               popupMessage: 'Hola, en que podemos ayudarte?',
+               showPopup: true, 
+               buttonImage: '<img src="img/whatsapp.svg" />',
+               position: "right" 
+
+           });
+       });
+  
